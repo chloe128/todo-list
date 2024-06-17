4a4a85f38aea4742789d2cbc2cec6e49b8d8e31f
chloe128 <chloemeng98@gmail.com>
2024-06-17 13:22:05 -0700

    added event listener when button is clicked

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 //access "add todo" button
 const button = document.getElementById("add-todo");
-
 console.log(button);
+
+//function to run when button is clicked
+function handleAddTodo() {
+  console.log("button is clicked");
+}
+//add event listener
+button.addEventListener("click", handleAddTodo);
